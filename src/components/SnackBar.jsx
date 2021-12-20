9a7d20251716e78be29a1e92c087c075aeb170f1
Pradnya5598 <rautpradnya223@gmail.com>
2021-12-20 19:01:26 +0530

    first commit

---
+++ src/components/SnackBar.jsx
@@ -0,0 +1,25 @@
+import {  Snackbar, makeStyles } from '@material-ui/core';
+import MuiAlert from '@material-ui/lab/Alert';
+const useStyles = makeStyles({
+    Snack: {
+        '& > *': {
+            color: '#fff',
+            backgroundColor: '#000000'
+         }
+    }
+})
+const SnackBar = ({ open, toggleSnack }) => {
+    const classes = useStyles();
+    const handleClose = () => {
+      toggleSnack(false);
+    }
+
+    return (
+      <Snackbar className={classes.snack} open={open} autoHideDuration={3000} onClose={handleClose}>
+                <MuiAlert severity="warning" onClose={handleClose}>
+                    Number of Images should be between 3 and 200
+                </MuiAlert>
+            </Snackbar>
+        )
+    }
+export default SnackBar; 
